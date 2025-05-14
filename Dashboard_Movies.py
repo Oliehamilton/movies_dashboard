e1ed15336be38eefcb2014d8ecffc2ef36ec61ed
DMonteiro <138933617+Oliehamilton@users.noreply.github.com>
2025-05-14 19:07:12 +0100

    Update Dashboard_Movies.py

--- Dashboard_Movies.py
+++ Dashboard_Movies.py
@@ -476,7 +476,7 @@ if selected_user:
                             """, unsafe_allow_html=True)
 
             # Add vertical spacing between rows
-            # st.markdown("<div style='margin-top: 1.5rem;'></div>", unsafe_allow_html=True)
+            st.markdown("<br><br>", unsafe_allow_html=True)
             
             # Second row with exactly 2 recommendations centered
             if len(top_recs) > 3:
