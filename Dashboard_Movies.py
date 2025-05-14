d13d8e31c65ca98de60b5c4417ec53f44003ac33
DMonteiro <138933617+Oliehamilton@users.noreply.github.com>
2025-05-14 17:17:37 +0100

    Update Dashboard_Movies.py

--- Dashboard_Movies.py
+++ Dashboard_Movies.py
@@ -12,7 +12,7 @@ import plotly.graph_objects as go
 
 # --- Page Config ---
 st.set_page_config(
-    page_title="Movie Dashboard",
+    page_title="Movie Dashboard-FUCK YOU",
     layout="wide",
     initial_sidebar_state="expanded"
 )
